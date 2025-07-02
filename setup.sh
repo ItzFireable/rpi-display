b14ded805d19f9e7f03249e03b3ae1dafa4ccb5d
ItzFireable <fireableplayz@gmail.com>
2025-07-02 12:46:00 +0300

    Additional fixes

--- setup.sh
+++ setup.sh
@@ -1,9 +1,9 @@
-sudo apt-get install pigpio
+apt-get install pigpio
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.3/install.sh | bash
 export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" --no-use
 nvm install 20
 
-sudo npm install -g n pigpio
-sudo n 20
+npm install -g n pigpio
+n 20
 npm install
