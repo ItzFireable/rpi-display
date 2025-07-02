e56fd454309077a13284642d6fbd0ac939680921
ItzFireable <fireableplayz@gmail.com>
2025-07-02 12:42:38 +0300

    Typo

--- setup.sh
+++ setup.sh
@@ -2,7 +2,7 @@ sudo apt-get install pigpio
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.3/install.sh | bash
 export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" --no-use
-nvm use 20
+nvm install 20
 
 sudo npm install -g n pigpio
 sudo n 20
