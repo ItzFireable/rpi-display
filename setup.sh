391878e1fa67c448645416ed077a979a9b5e4f78
ItzFireable <fireableplayz@gmail.com>
2025-07-02 12:41:18 +0300

    Update setup

--- setup.sh
+++ setup.sh
@@ -1,6 +1,9 @@
-sudo apt-get install nodejs nvm pigpio
+sudo apt-get install pigpio
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.3/install.sh | bash
+export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" --no-use
+nvm use 20
+
 sudo npm install -g n pigpio
 sudo n 20
-
-git clone https://github.com/ItzFireable/rpi-display && cd rpi-display
 npm install
